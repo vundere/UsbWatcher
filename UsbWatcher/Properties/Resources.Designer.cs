a81c0520e44dbb2cc9921fdbd2bab0f140c52651
Olav Hobberstad <colddimension@gmail.com>
2018-11-21 15:50:04 +0100

    removed usb id file from resources

--- UsbWatcher/Properties/Resources.Designer.cs
+++ UsbWatcher/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace UsbWatcher.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] usb {
-            get {
-                object obj = ResourceManager.GetObject("usb", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
